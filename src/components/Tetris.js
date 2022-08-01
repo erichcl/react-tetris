250c10e1f11133824bc5da6479309ebc0a4dec14
Erich Lersch <erich.lersch@immutable.com>
2022-08-01 19:13:30 -0300

    Start of the preview of the next piece

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,7 @@
 import './Tetris.css';
 import Board from './Board';
 import GameStats from './GameStats';
+import Previews from './Previews';
 
 import { useBoard } from '../hooks/useBoard';
 import { useGameStats } from '../hooks/useGameStats';
@@ -8,11 +9,13 @@ import { useGameStats } from '../hooks/useGameStats';
 const Tetris = ({ rows, columns, setGameOver }) => {
   const [board] = useBoard(rows, columns);
   const [gameStats, addLinesCleared] = useGameStats();
+  const player = { tetrominoes: [] };
 
   return (
     <div className="Tetris">
       <Board board={board} />
       <GameStats gameStats={gameStats} />
+      <Previews tetrominoes={player.tetrominoes} />
     </div>
   );
 };
